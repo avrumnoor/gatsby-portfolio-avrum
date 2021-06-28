fc373ec4f97060356e977fcd7a00d32b492a76ca
Avrum Noor <avrumnoor@stanford.edu>
2021-06-28 21:06:55 +0700

    Update
    update

--- src/components/layout/navbar/index.jsx
+++ src/components/layout/navbar/index.jsx
@@ -18,9 +18,9 @@ const Navbar = (AVRUM) => (
           </NavbarLogo> 
           <div className="main-navigation">
             <NavbarList>
-              <li><Link to="/" className="lined-link" activeClassName="active"> <User /> <span> About </span> </Link></li> 
-              <li><Link to="/repositories" className="lined-link" activeClassName="active"> <GitHub /> <span> GitHub </span> </Link></li>  
-              <li><Link to="/works" className="lined-link" activeClassName="active"> <Briefcase /> <span> Works </span> </Link></li>  
+              <li><Link to="/" className="lined-link" activeClassName="active"> <User /> <span> About </span> </Link></li>  
+              <li><Link to="/works" className="lined-link" activeClassName="active"> <Briefcase /> <span> Works </span> </Link></li>
+              <li><Link to="/repositories" className="lined-link" activeClassName="active"> <GitHub /> <span> Repos </span> </Link></li>
               <li><Link to="/blog" className="lined-link" activeClassName="active"> <Feather /> <span> Blog </span> </Link></li>
             </NavbarList>
           </div>
